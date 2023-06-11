7c6d4ba4488fc6db96f4561a5ee06a3b86bf9b3c
coffeekitkat <82024111+coffeekitkat@users.noreply.github.com>
2023-06-11 23:20:50 +0800

    setup web socket on adonis js backend

--- server/providers/AppProvider.ts
+++ server/providers/AppProvider.ts
@@ -13,6 +13,9 @@ export default class AppProvider {
   }
 
   public async ready () {
+    if (this.app.environment === 'web') {
+      await import('../start/socket')
+    }
     // App is ready
   }
 
